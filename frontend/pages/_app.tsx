167af6a32b08e63fee689a2102d138ed33b06e9b
Ronaldop333 <ronaldop333@hotmail.com>
2025-05-02 22:07:35 -0300

    Adiciona favicon e título ao HTML

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,1 +1,14 @@
-import "../styles.css"; export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
+import "../styles.css"; export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }import Head from "next/head";
+import "../styles.css";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>AiTradeX - New Experience in Trade AI</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
