5feb7d5f4e29a8bf0e64fd86bed26aad4b68719d
Ronaldop333 <ronaldop333@hotmail.com>
2025-05-04 14:53:46 -0300

    fix: remove duplicação de const PORT

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000; // Railway define a PORT dinamicamente
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('API funcionando na Railway!');
 });
 
 app.get("/api/status", (req, res) => {
-    res.json({ status: "online", timestamp: Date.now() });
-  });
-  
-  const PORT = process.env.PORT;
+  res.json({ status: "online", timestamp: Date.now() });
+});
 
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`🚀 Servidor rodando na porta ${PORT}`);
-  });
-  
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+});
