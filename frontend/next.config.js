19d69c28aa80cfc2de5c0c2e788579a4b93a8375
Ronaldop333 <ronaldop333@hotmail.com>
2025-05-04 19:44:02 -0300

    chore: remove reactStrictMode e aplica rewrites no next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,14 @@
-module.exports = { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://tradegen-marketplace-production.up.railway.app/api/:path*',
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
