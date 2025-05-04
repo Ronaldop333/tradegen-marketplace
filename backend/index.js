de2ef449a68e91ef843d76867d052ac0abf8e9d2
Ronaldop333 <ronaldop333@hotmail.com>
2025-05-04 14:23:12 -0300

    feat: ativa rota GET /api/status para testes com Vercel

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('API funcionando na Railway!');
 });
 
+app.get("/api/status", (req, res) => {
+    res.json({ status: "online", timestamp: Date.now() });
+  });
+  
+
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
