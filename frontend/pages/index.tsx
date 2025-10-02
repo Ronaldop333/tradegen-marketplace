2e9a1149babea804486db64871775c8fe240fe27
Ronaldop333 <35382410+Ronaldop333@users.noreply.github.com>
2025-10-02 16:49:58 -0300

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
 
       <p style={{ maxWidth: "700px", textAlign: "center", fontSize: "1.1rem" }}>
         Desenvolvemos soluções inteligentes para o ambiente de trading.
-       <br Automatizamos operações, integramos ferramentas, criamos >
+        Automatizamos operações, integramos ferramentas, criamos
         sistemas personalizados para gestão de capital, risco e controle emocional.
       </p>
 
